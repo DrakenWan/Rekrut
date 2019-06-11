d5ebb6cc83c69e7e5d43be30e783ffe3a0bc1aac
DrakenWan <kartikaykaul13@gmail.com>
2019-06-11 17:18:00 +0530

    Update login.js
    I added a sophisticated and annoying animation through the js if user leaves the fields blank

--- js/login.js
+++ js/login.js
@@ -45,6 +45,7 @@ document.getElementById("userdetails").addEventListener("submit", function(event
         //javascript events are skipping unless they request a user input
         //probably because this is an async function
         //alert("The fields are empty. Fill them before pressing login.");
+        event.preventDefault();
         var msg = document.getElementById("error-msg");
         if(uname.value == "" && pwd.value == "") msg.innerText = "Please fill the fields left blank."
         msg.style = "display: block;\
@@ -55,7 +56,38 @@ document.getElementById("userdetails").addEventListener("submit", function(event
                     border-left: 5px solid red;\
                     background-color:  #f3f1f1;\
                     transition: 0.4s;";
-        event.preventDefault();
+        
+        //static variable use
+        if(typeof this.counter == 'undefined')
+        {
+            this.counter = 0;
+        }
+        else
+        {
+            this.counter++;
+            msg.style = "font-size: "+(12+this.counter).toString() +"px;\
+                         color: red;\
+                         text-align: center;\
+                         text-shadow: 0.5px 0.5px 2px red;\
+                         border-left: 5px solid red;\
+                         background-color:  #f3f1f1;\
+                         transition: 0.5s;";
+            
+            // a simple yet annoying animation for defaulters
+            setTimeout(function()
+            {
+                msg.style = "display: block;\
+                    font-size: 12px;\
+                    color: red;\
+                    text-align: center;\
+                    text-shadow: 0.5px 0.5px 2px red;\
+                    border-left: 5px solid red;\
+                    background-color:  #f3f1f1;\
+                    transition: 0.4s;";
+            },
+            200);
+        }
+        
     }
 });
 
