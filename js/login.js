0bbb32634a3747dd3f1022c4dee1f1a8c678f0ad
DrakenWan <kartikaykaul13@gmail.com>
2019-06-11 15:09:42 +0530

    Validation improved
    Improved the validation in the login form

--- js/login.js
+++ js/login.js
@@ -26,7 +26,7 @@ function getById(id)
     return element;
 }
 
-document.getElementById("login").addEventListener("click", function()
+document.getElementById("userdetails").addEventListener("submit", function(event)
 {
     var uname = getById("uname");
     var pwd = getById("pwd");
@@ -44,7 +44,18 @@ document.getElementById("login").addEventListener("click", function()
     {
         //javascript events are skipping unless they request a user input
         //probably because this is an async function
-        alert("The fields are empty. Fill them before pressing login.");
+        //alert("The fields are empty. Fill them before pressing login.");
+        var msg = document.getElementById("error-msg");
+        if(uname.value == "" && pwd.value == "") msg.innerText = "Please fill the fields left blank."
+        msg.style = "display: block;\
+                    font-size: 12px;\
+                    color: red;\
+                    text-align: center;\
+                    text-shadow: 0.5px 0.5px 2px red;\
+                    border-left: 5px solid red;\
+                    background-color:  #f3f1f1;\
+                    transition: 0.4s;";
+        event.preventDefault();
     }
 });
 
