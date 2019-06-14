f51df375e3189eb62f33ef028a41fe4dfacfea78
DrakenWan <kartikaykaul13@gmail.com>
2019-06-14 11:45:21 +0530

    Errors handled
    > login.js : producing unwanted console.error messages. They were not fatal for the extension.
    > signup_user.js : This file was not registering the credentials into the mongodb database. I have removed the causes of error and it works perfectly in every condition now.
    > If any issues comes up please inform me.

--- js/login.js
+++ js/login.js
@@ -15,10 +15,7 @@ document.getElementById("closebtn").addEventListener("click", function()
 {
     //closing the slider request sent
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id,{todo: "toggle"}, function(resp)
-        {
-            console.log(resp);
-        });
+        chrome.tabs.sendMessage(tabs[0].id,{todo: "toggle"});
     })
 });
 
@@ -40,10 +37,7 @@ document.getElementById("userdetails").addEventListener("submit", function(event
 
         chrome.tabs.query({active: true, currentWindow: true}, function(tab)
         {
-            chrome.tabs.sendMessage(tab[0].id, {todo: "loggingin", data: rekrut}, function(msg)
-            {
-                console.log(msg);
-            });
+            chrome.tabs.sendMessage(tab[0].id, {todo: "loggingin", data: rekrut});
         });
     }
     else
